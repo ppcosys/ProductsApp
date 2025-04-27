1e19a6006c344019294e8a142306136c86af84ca
Peter <ppcosys@gmail.com>
2025-04-27 11:15:38 +0200

    Adding Home Component frontend

--- client/src/app/_services/products.service.ts
+++ client/src/app/_services/products.service.ts
@@ -1,14 +1,37 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface Product {
+  id: number;
+  kod: string;
+  nazwa: string;
+  cena: number;
+}
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
   private http = inject(HttpClient);
-  baseUrl = 'http://localhost:5000/api/';
+  baseUrl = 'http://localhost:5000/api/products';
   
-  addProduct(model: any) {
-    return this.http.post(this.baseUrl + 'products', model);
+  constructor() {}
+
+  getProducts(): Observable<Product[]>{
+    return this.http.get<Product[]>(this.baseUrl);
+  }
+
+  addProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
+  }
+
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseUrl}/${id}`);
+  }
+
+  updateProduct(product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.baseUrl}/${product.id}`, product);
   }
 }
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { Product, ProductsService } from '../_services/products.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-  products = [
-    { id: 12, kod: 'P012', nazwa: 'Laptop Home', cena: 2999 }
-  ]
+export class HomeComponent implements OnInit {
+  private productsService = inject(ProductsService);
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts() {
+    this.productsService.getProducts().subscribe({
+      next: (response) => this.products = response,
+      error: (err) => console.error(err)
+    });
+  }
+
+  onAddProduct() {
+    const newProduct: Product = { id: 0, kod: 'P999', nazwa: 'Nowy Produkt', cena: 1234 };
+    this.productsService.addProduct(newProduct).subscribe(() => this.loadProducts());
+  }
+
+  onDeleteProduct(id: number) {
+    this.productsService.deleteProduct(id).subscribe(() => this.loadProducts());
+  }
+
+  onUpdateProduct(product: Product) {
+    const updatedProduct = { ...product, nazwa: 'Zmieniona Nazwa' };
+    this.productsService.updateProduct(updatedProduct).subscribe(() => this.loadProducts());
+  }
+
 }
+
